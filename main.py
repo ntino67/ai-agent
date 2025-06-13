0ff8ff8a897f1282009c4df748372f0aab2268ff
ntino67 <matteo.heidelberger@viacesi.fr>
2025-06-13 11:09:08 +0200

    Added a verbose arg, implemented lists to retain messages for later

--- main.py
+++ main.py
@@ -3,22 +3,46 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-try:
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=sys.argv[1],
-    )
-except:
-    print("Provide a prompt first e.g.: `python3 main.py 'Hello'`")
-    sys.exit(1)
+user_prompt = sys.argv[1]
 
-print(response.text)
+args = sys.argv[2:]
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
+
+def main():
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+        )
+    except:
+        print("Provide a prompt first e.g.: `python3 main.py 'Hello'`")
+        sys.exit(1)
+
+    prompt_token = response.usage_metadata.prompt_token_count
+    response_token = response.usage_metadata.candidates_token_count
+
+    print(response.text)
+
+    if argument_reader(args):
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_token}")
+        print(f"Response tokens: {response_token}")
+
+
+def argument_reader(args):
+    if "--verbose" in args:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    main()
